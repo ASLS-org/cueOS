df46f358629f0c4ef1c52688311ae0a108cf4819
root <root@kali.asls.com>
2020-02-18 15:06:49 +0100

    [~] DMX512 driver on hold -> not enough ram, waiting for external SPI ram to pursue development of the app

--- apps/DMX512/src/DMX512_engine.c
+++ apps/DMX512/src/DMX512_engine.c
@@ -1,103 +1,103 @@
-#include "DMX512_engine.h"
-#include "DMX512_fixture.h"
-#include "DMX512_defs.h"
-#include "cmsis_os.h"
-
-/** Private functions declaration */
-static DMX512_engine_s *this;
-static DMX512_engine_err_e DMX512_engine_patch(uint16_t id, uint16_t chStart, uint16_t chStop);
-static DMX512_engine_err_e DMX512_engine_unpatch(uint16_t id);
-static DMX512_engine_err_e DMX512_engine_checkPatch(uint16_t chStart, uint16_t chStop);
-static uint16_t DMX512_engine_getFixtureIndex(uint16_t id);
-
-
-DMX512_engine_s *DMX512_engine_init(void){
-
-	this = pvPortMalloc(sizeof(DMX512_engine_s));
-
-	this->_fixtureCount = 0;
-//	this->_sceneCount 	= 0;
-//	this->_effectCount	= 0;
-	this->patch 		= DMX512_engine_patch;
-	this->unpatch 		= DMX512_engine_unpatch;
-
-	return this;
-}
-
-static DMX512_engine_err_e DMX512_engine_patch(uint16_t id, uint16_t chStart, uint16_t chStop){
-
-	DMX512_engine_err_e err = DMX512_ENGINE_OK;
-
-	if(err == DMX512_ENGINE_OK){
-		err = DMX512_engine_checkPatch(chStart, chStop);
-		if(err == DMX512_ENGINE_OK){
-			DMX512_fixture_s fixture = DMX512_fixture_init(id , chStart, chStop);
-			this->_fixtures = pvPortRealloc(this->_fixtures, sizeof(DMX512_fixture_s) * (this->_fixtureCount + 1));
-			this->_fixtures[this->_fixtureCount] = fixture;
-			this->_fixtureCount++;
-		}
-	}
-
-	return err;
-
-}
-
-DMX512_engine_err_e DMX512_engine_unpatch(uint16_t id){
-
-	DMX512_engine_err_e err = DMX512_ENGINE_OK;
-	int16_t index = DMX512_engine_getFixtureIndex(id);
-
-	if(index >= 0){
-		for(uint16_t i=0; i< this->_fixtureCount; i++){
-			if(i >= index){
-				this->_fixtures[i] = this->_fixtures[i+1];
-			}
-		}
-		this->_fixtureCount--;
-		this->_fixtures = pvPortRealloc(this->_fixtures, sizeof(DMX512_fixture_s) * (this->_fixtureCount));
-	}else{
-		err = DMX512_FIXTURE_UNKNW;
-	}
-
-	return err;
-
-}
-
-static DMX512_engine_err_e DMX512_engine_checkPatch(uint16_t chStart, uint16_t chStop){
-
-	DMX512_engine_err_e err = DMX512_ENGINE_OK;
-
-	if(chStop < chStart){
-		err  = DMX512_CHANNEL_ADDRESS_INTERVAL_NEG;
-	}else if(chStart >= DMX512_CHANNEL_ADDRESS_MIN && chStop <= DMX512_CHANNEL_ADDRESS_MAX){
-		for(uint16_t i=0; i<this->_fixtureCount; i++){
-			if(chStart <= this->_fixtures[i].chStop && this->_fixtures[i].chStart <= chStop){
-				err = DMX512_CHANNEL_ADDRESS_DUP;
-				break;
-			}
-		}
-	}else{
-		err  = DMX512_CHANNEL_ADDRESS_OUT_OF_BOUNDS;
-	}
-
-	return err;
-
-}
-
-uint16_t DMX512_engine_getFixtureIndex(uint16_t id){
-	for(uint16_t i=0; i<this->_fixtureCount; i++){
-		if(id == this->_fixtures[i].id){
-			return id;
-		}
-	}
-	return -1;
-}
-
-DMX512_fixture_s *DMX512_engine_getFixture(uint16_t id){
-	for(uint16_t i=0; i<this->_fixtureCount; i++){
-		if(id == this->_fixtures[i].id){
-			return &this->_fixtures[i];
-		}
-	}
-	return NULL;
-}
+//#include "DMX512_engine.h"
+//#include "DMX512_fixture.h"
+//#include "DMX512_defs.h"
+//#include "cmsis_os.h"
+//
+///** Private functions declaration */
+//static DMX512_engine_s *this;
+//static DMX512_engine_err_e DMX512_engine_patch(uint16_t id, uint16_t chStart, uint16_t chStop);
+//static DMX512_engine_err_e DMX512_engine_unpatch(uint16_t id);
+//static DMX512_engine_err_e DMX512_engine_checkPatch(uint16_t chStart, uint16_t chStop);
+//static uint16_t DMX512_engine_getFixtureIndex(uint16_t id);
+//
+//
+//DMX512_engine_s *DMX512_engine_init(void){
+//
+//	this = pvPortMalloc(sizeof(DMX512_engine_s));
+//
+//	this->_fixtureCount = 0;
+////	this->_sceneCount 	= 0;
+////	this->_effectCount	= 0;
+//	this->patch 		= DMX512_engine_patch;
+//	this->unpatch 		= DMX512_engine_unpatch;
+//
+//	return this;
+//}
+//
+//static DMX512_engine_err_e DMX512_engine_patch(uint16_t id, uint16_t chStart, uint16_t chStop){
+//
+//	DMX512_engine_err_e err = DMX512_ENGINE_OK;
+//
+//	if(err == DMX512_ENGINE_OK){
+//		err = DMX512_engine_checkPatch(chStart, chStop);
+//		if(err == DMX512_ENGINE_OK){
+//			DMX512_fixture_s fixture = DMX512_fixture_init(id , chStart, chStop);
+//			this->_fixtures = pvPortRealloc(this->_fixtures, sizeof(DMX512_fixture_s) * (this->_fixtureCount + 1));
+//			this->_fixtures[this->_fixtureCount] = fixture;
+//			this->_fixtureCount++;
+//		}
+//	}
+//
+//	return err;
+//
+//}
+//
+//DMX512_engine_err_e DMX512_engine_unpatch(uint16_t id){
+//
+//	DMX512_engine_err_e err = DMX512_ENGINE_OK;
+//	int16_t index = DMX512_engine_getFixtureIndex(id);
+//
+//	if(index >= 0){
+//		for(uint16_t i=0; i< this->_fixtureCount; i++){
+//			if(i >= index){
+//				this->_fixtures[i] = this->_fixtures[i+1];
+//			}
+//		}
+//		this->_fixtureCount--;
+//		this->_fixtures = pvPortRealloc(this->_fixtures, sizeof(DMX512_fixture_s) * (this->_fixtureCount));
+//	}else{
+//		err = DMX512_FIXTURE_UNKNW;
+//	}
+//
+//	return err;
+//
+//}
+//
+//static DMX512_engine_err_e DMX512_engine_checkPatch(uint16_t chStart, uint16_t chStop){
+//
+//	DMX512_engine_err_e err = DMX512_ENGINE_OK;
+//
+//	if(chStop < chStart){
+//		err  = DMX512_CHANNEL_ADDRESS_INTERVAL_NEG;
+//	}else if(chStart >= DMX512_CHANNEL_ADDRESS_MIN && chStop <= DMX512_CHANNEL_ADDRESS_MAX){
+//		for(uint16_t i=0; i<this->_fixtureCount; i++){
+//			if(chStart <= this->_fixtures[i].chStop && this->_fixtures[i].chStart <= chStop){
+//				err = DMX512_CHANNEL_ADDRESS_DUP;
+//				break;
+//			}
+//		}
+//	}else{
+//		err  = DMX512_CHANNEL_ADDRESS_OUT_OF_BOUNDS;
+//	}
+//
+//	return err;
+//
+//}
+//
+//uint16_t DMX512_engine_getFixtureIndex(uint16_t id){
+//	for(uint16_t i=0; i<this->_fixtureCount; i++){
+//		if(id == this->_fixtures[i].id){
+//			return id;
+//		}
+//	}
+//	return -1;
+//}
+//
+//DMX512_fixture_s *DMX512_engine_getFixture(uint16_t id){
+//	for(uint16_t i=0; i<this->_fixtureCount; i++){
+//		if(id == this->_fixtures[i].id){
+//			return &this->_fixtures[i];
+//		}
+//	}
+//	return NULL;
+//}
--- apps/DMX512/src/DMX512_scene.c
+++ apps/DMX512/src/DMX512_scene.c
@@ -1,221 +1,106 @@
-//#include "DMX512_scene.h"
-//#include "DMX512_fixture_pool.h"
-//#include "cmsis_os.h"
-//
-//DMX512_fixture_preset_s *DMX512_fixture_preset_init(uint16_t id, uint8_t *values);
-//
-//
-//DMX512_scene_s *DMX512_scene_init(uint16_t id){
-//	DMX512_scene_s *scene = pvPortMalloc(sizeof(DMX512_scene_s));
-//	scene->id = id;
-//	scene->_fixture_presets = NULL;
-//	return scene;
-//}
-//
-////static DMX512_fixture_s *DMX512_scene_get(uint16_t index){
-////
-////	uint16_t hash_index 		= DMX512_scene_hash(index);
-////	DMX512_fixture_s *current	= this->_hash_table[hash_index];
-////
-////	while(current != NULL){
-////		if(current->id == index){
-////			return current;
-////		}
-////		current = current->_next;
-////	}
-////
-////	return NULL;
-////
-////}
-////
-//DMX512_fixture_preset_s *DMX512_fixture_preset_init(uint16_t id, uint8_t *values){
-//
-//
-//	DMX512_fixture_preset_s *preset = pvPortMalloc(sizeof(DMX512_fixture_preset_s));
-//	DMX512_fixture_s *fixture = DMX512_fixture_pool_get(id);
-//
-//	if(fixture != NULL){
-//		preset->fixture_id		= fixture->id;
-//		preset->fixture_values 	= pvPortMalloc(sizeof(uint8_t) * fixture->chCount);
-//		preset->_next = NULL;
-//		memcpy(preset->fixture_values, values, fixture->chCount);
-//	}else{
-//		vPortFree(preset);
-//	}
-//
-//	return preset;
-//
-//}
-//
-//DMX512_engine_err_e DMX512_scene_add(DMX512_scene_s *this, uint16_t id, uint8_t *values){
-//
-//	DMX512_engine_err_e err 		 	= DMX512_ENGINE_OK;
-//	DMX512_fixture_preset_s *current 	= NULL;
-//	DMX512_fixture_preset_s *preset 	= DMX512_fixture_preset_init(id, values);
-//
-//	if(err == DMX512_ENGINE_OK){
-//		if(preset == NULL){
-//			err = DMX512_FIXTURE_UNKNW;
-//		}else{
-//			if(this->_fixture_presets == NULL){
-//				this->_fixture_presets = preset;
-//			}else{
-//				current = this->_fixture_presets ;
-//				while(current->_next != NULL){
-//					current = current->_next;
-//				}
-//				current->_next = preset;
-//			}
-//		}
-//	}
-//
-//	return err;
-//
-//}
-////
-////void DMX512_scene_start(DMX512_scene_s *this){
-////
-////	DMX512_fixture_preset_s *current = NULL;
-////
-////	current = this->_fixture_presets;
-////	while(current != NULL){
-////		for(uint16_t i=0; i< current->fixture->chCount; i++){
-////			current->fixture->set(current->fixture, i, current->values[i]);
-////		}
-////		current = current->_next;
-////	}
-////
-////}
-////
-////
-////static DMX512_engine_err_e DMX512_scene_clr(uint16_t index){
-////
-////	DMX512_engine_err_e err = DMX512_FIXTURE_UNKNW;
-////
-////	uint16_t hash_index 		= DMX512_scene_hash(index);
-////	DMX512_fixture_s *prev, *current	= this->_hash_table[hash_index];
-////
-////	while(current != NULL){
-////		if(current->id == index){
-////			err = DMX512_ENGINE_OK;
-////			prev->_next = current->_next;
-////			current->free(current);
-////			break;
-////		}
-////		prev = current;
-////		current = current->_next;
-////	}
-////
-////	return err;
-////
-////}
-//
-////
-////static DMX512_engine_err_e DMX512_scene_addPreset(DMX512_scene_s *this, uint16_t id, uint16_t *channels, uint8_t *values);
-////static DMX512_engine_err_e DMX512_scene_updPreset(DMX512_scene_s *this, uint16_t id, uint16_t *channels, uint8_t *values);
-////static DMX512_engine_err_e DMX512_scene_clrPreset(DMX512_scene_s *this, uint16_t fixtureId);
-////static void DMX512_scene_trigger(DMX512_scene_s *this);
-////static int16_t DMX512_scene_getPresetIndex(DMX512_scene_s *this, uint16_t presetId);
-////
-////
-////DMX512_scene_s *DMX512_scene_init(uint16_t id){
-////
-////	DMX512_scene_s *scene = pvPortMalloc(sizeof(DMX512_scene_s));
-////
-////	scene->id = id;
-////	scene->presetCount = 0;
-////	scene->presets = NULL;
-////
-////	scene->add 		= DMX512_scene_addPreset;
-////	scene->upd		= DMX512_scene_updPreset;
-////	scene->clr		= DMX512_scene_clrPreset;
-////	scene->trigger	= DMX512_scene_trigger;
-////
-////	return scene;
-////
-////}
-////
-////static DMX512_engine_err_e DMX512_scene_addPreset(DMX512_scene_s *this, uint16_t id, uint16_t *channels, uint8_t *values){
-////
-////	DMX512_engine_err_e err = DMX512_ENGINE_OK;
-////
-////	if(DMX512_scene_getPresetIndex(this, id) == DMX512_PRESET_NOT_FOUND){
-////		this->presetCount ++;
-////		this->presets = pvPortRealloc(this->presets, this->presetCount * sizeof(DMX512_preset_s));
-////		err = DMX512_preset_init(this->presets[this->presetCount], id, channels, values);
-////		if(err != DMX512_ENGINE_OK){
-////			DMX512_scene_clrPreset(this, id);
-////		}
-////	}else{
-////		err = DMX512_PRESET_DUP;
-////	}
-////
-////	return err;
-////
-////}
-////
-////static DMX512_engine_err_e DMX512_scene_updPreset(DMX512_scene_s *this, uint16_t id, uint16_t *channels, uint8_t *values){
-////
-////	DMX512_engine_err_e err = DMX512_ENGINE_OK;
-////	uint16_t index = DMX512_scene_getPresetIndex(this, id);
-////
-////	if(id != DMX512_PRESET_NOT_FOUND){
-////		err = this->presets[index]->upd(this->presets[index], channels, values);
-////	}else{
-////		err = DMX512_PRESET_UNKNW;
-////	}
-////
-////	return err;
-////
-////}
-////
-////static DMX512_engine_err_e DMX512_scene_clrPreset(DMX512_scene_s *this, uint16_t id){
-////
-////	DMX512_engine_err_e err = DMX512_ENGINE_OK;
-////	int16_t index = DMX512_scene_getPresetIndex(this,id);
-////
-////	if(id != DMX512_PRESET_UNKNW){
-////
-////		DMX512_preset_s **tmpPresets = pvPortMalloc((this->presetCount - 1) * sizeof(DMX512_preset_s));
-////
-////		for(uint16_t i=0; i<this->presetCount; i++){
-////			if(i < index){
-////				tmpPresets[i] = this->presets[i];
-////			}else{
-////				tmpPresets[i] = this->presets[i+1];
-////			}
-////		}
-////
-////		this->presetCount--;
-////		memcpy(this->presets, tmpPresets, this->presetCount * sizeof(DMX512_preset_s));
-////		free(tmpPresets);
-////
-////	}else{
-////		err = DMX512_PRESET_UNKNW;
-////	}
-////
-////
-////	return err;
-////
-////}
-////
-////static void DMX512_scene_trigger(DMX512_scene_s *this){
-////	for(uint16_t i = 0; i< this->presetCount; i++){
-////		for(uint16_t j = 0; j< this->presets[i]->fixture->chCount; j++){
-////			this->presets[i]->set(this->presets[i]);
-////		}
-////	}
-////}
-////
-////
-////static int16_t DMX512_scene_getPresetIndex(DMX512_scene_s *this, uint16_t presetId){
-////
-////	for(uint16_t i=0; i< (sizeof(this->presets)/sizeof(DMX512_preset_s)); i++){
-////		if(presetId == this->presets[i]->id){
-////			return i;
-////		}
-////	}
-////
-////	return DMX512_PRESET_UNKNW;
-////
-////}
+#include "DMX512_scene.h"
+#include "DMX512_fixture_pool.h"
+#include "cmsis_os.h"
+
+static int16_t _DMX512_scene_search(DMX512_scene_s *this, uint16_t id);
+
+DMX512_scene_s DMX512_scene_init(uint16_t id, uint16_t fadein_time, uint16_t fadeout_time){
+	DMX512_scene_s scene;
+	scene.id = id;
+	scene.fadein_time = fadein_time;
+	scene.fadeout_time = fadeout_time;
+	scene._fixture_presets = NULL;
+	return scene;
+}
+
+/**
+ * Adds a fixture preset instance into the scene
+ *
+ * @param this pointer to the scene instance
+ * @param id the fixture's idendifier
+ * @param values the list of values to be stored within the scene
+ * @return DMX512_engine_err_e error code following the function call
+ */
+DMX512_engine_err_e DMX512_scene_add(DMX512_scene_s *this, uint16_t id, uint8_t *values){
+
+	DMX512_engine_err_e err   = DMX512_ENGINE_OK;
+	DMX512_fixture_s *fixture = DMX512_fixture_pool_get(id);
+
+	if(fixture == NULL){
+		DMX512_fixture_preset_s preset;
+		preset.id = fixture->id;
+		preset._channel_values = pvPortMalloc(sizeof(uint8_t) * (fixture->ch_stop - fixture->ch_start + 1));
+		this->_fixture_presets = pvPortRealloc(this->_fixture_presets, sizeof(DMX512_fixture_preset_s) * this->_preset_count + 1);
+		this->_fixture_presets[this->_preset_count] = preset;
+		this->_preset_count++;
+	}else{
+		err = DMX512_PRESET_DUP;
+	}
+
+	return err;
+
+}
+
+
+/**
+ * Deletes a fixture preset instance from the scene
+ *
+ * @param id the fixture preset's idendifier
+ * @return DMX512_engine_err_e error code following the function call
+ */
+DMX512_engine_err_e DMX512_scene_del(DMX512_scene_s *this, uint16_t id){
+
+	DMX512_engine_err_e err = DMX512_ENGINE_OK;
+	int16_t index = _DMX512_scene_search(this, id);
+
+	if(index >= 0){
+		for(uint16_t i=0; i< this->_preset_count; i++){
+			if(i > index){
+				this->_fixture_presets[i-1] = this->_fixture_presets[i];
+			}
+		}
+		this->_preset_count--;
+		vPortFree(this->_fixture_presets[index]._channel_values);
+		this->_fixture_presets = pvPortRealloc(this->_fixture_presets, sizeof(DMX512_fixture_s) * (this->_preset_count));
+	}else{
+		err = DMX512_PRESET_UNKNW;
+	}
+
+	return err;
+
+}
+
+/**
+ * Gets a fixture instance from the pool
+ *
+ * @param id the fixture preset identifier
+ * @return *DMX512_fixture_preset_s pointer to the fixture preset instance
+ */
+DMX512_fixture_preset_s *DMX512_scene_get(DMX512_scene_s *this, uint16_t id){
+	int16_t index = _DMX512_scene_search(this, id);
+	if(index >= 0){
+		return &this->_fixture_presets[index];
+	}else{
+		return NULL;
+	}
+
+}
+
+
+/**
+ * Finds the array index of a fixture
+ *
+ * @param id the fixture's identifier
+ * @return int16_t the array index of the fixture.
+ * -1 is returned if the fixture couldn't be found
+ */
+static int16_t _DMX512_scene_search(DMX512_scene_s *this, uint16_t id){
+	for(uint16_t i=0; i<this->_preset_count; i++){
+		if(id == this->_fixture_presets[i].id){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
