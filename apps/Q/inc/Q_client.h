b593185f79c65641c77607471fab7e5b28ac0dba
root <root@kali.asls.com>
2020-02-19 17:35:15 +0100

    [~] - listening for incoming UDP packets now working multicast

--- apps/Q/inc/Q_client.h
+++ apps/Q/inc/Q_client.h
@@ -1,20 +1,28 @@
-/*
- * Q_client.h
- *
- *  Created on: 18 Feb 2020
- *      Author: root
- */
 
 #ifndef _Q_CLIENT_H_
 #define _Q_CLIENT_H_
 
+#include "lwip/api.h"
 #include "Q_defs.h"
+#include "net.h"
 
+#define Q_PORT 5214
+#define Q_CLIENT_DEFAULT {NULL, NULL, {}, {}, Q_CLIENT_UNBOUND}
+
+typedef enum{
+	Q_CLIENT_UNBOUND,
+	Q_CLIENT_BOUND
+}Q_client_state_e;
 
 typedef struct{
-	struct udp_pcb *pcb;
-
+	struct netconn *conn;
+	struct netbuf *buf;
+	ip4_addr_t ip_addr;
+	ip4_addr_t mcast_addr;
+	ip4_addr_t remote_addr;
+	Q_client_state_e state;
 }Q_client_s;
 
+Q_client_err_e Q_client_init(void);
 
-#endif /* APPS_Q_INC_Q_CLIENT_H_ */
+#endif
