8b6f3510dbe2470d2b95ee59a690b99b1b1e0e22
root <root@kali.asls.com>
2020-02-19 19:26:33 +0100

    [~] - Finished UDP receive thread implementation. TODO: test for robustness

--- apps/Q/inc/Q_client.h
+++ apps/Q/inc/Q_client.h
@@ -10,9 +10,19 @@
 #define Q_CLIENT_DEFAULT {NULL, NULL, {}, {}, Q_CLIENT_UNBOUND}
 
 typedef enum{
+	Q_CLIENT_GROUPCFG_CTRL 	= 200,
+	Q_CLIENT_GROUPCFG_DMX 	= 201,
+	Q_CLIENT_GROUPCFG_PYRO 	= 202,
+	Q_CLIENT_GROUPCFG_MEDIA = 203,
+	Q_CLIENT_GROUPCFG_OTHER = 204
+}Q_client_groupcfg_e;
+
+typedef enum{
 	Q_CLIENT_UNBOUND,
 	Q_CLIENT_BOUND
 }Q_client_state_e;
+
+typedef void(*Q_client_recv_f)(uint8_t *data);
 
 typedef struct{
 	struct netconn *conn;
@@ -21,8 +31,9 @@ typedef struct{
 	ip4_addr_t mcast_addr;
 	ip4_addr_t remote_addr;
 	Q_client_state_e state;
+	Q_client_recv_f recv_clbck;
 }Q_client_s;
 
-Q_client_err_e Q_client_init(void);
+void Q_client_init(Q_client_groupcfg_e groupcfg, Q_client_recv_f recv_clbck);
 
 #endif
