67c758c211ba8538919de29f95a9154fa400e7a6
Timé Kadel <time.kadel@reseau.eseo.fr>
2020-03-03 17:58:11 +0100

    [~] - modified maximum heap size to work with FatFs LFN. TODO: consider lowering size

--- system/inc/FreeRTOSConfig.h
+++ system/inc/FreeRTOSConfig.h
@@ -60,7 +60,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)//2048)//128
-#define configTOTAL_HEAP_SIZE                    ((size_t)15360)//24576)//15360
+#define configTOTAL_HEAP_SIZE                    ((size_t)99000)//24576)//15360
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
