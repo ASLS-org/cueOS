4ed3027a23b58d07c866f2c2d13e2933d028d13d
Timé Kadel <time.kadel@reseau.eseo.fr>
2020-03-05 21:09:24 +0100

    [+] - Updated chaser structure to match QLSF file format

--- apps/DMX512/inc/DMX512_chaser.h
+++ apps/DMX512/inc/DMX512_chaser.h
@@ -4,25 +4,34 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include "DMX512_scene.h"
+#include "DMX512_chaser_step.h"
 #include "DMX512_defs.h"
 
-typedef struct DMX512_chaser DMX512_chaser_s;
+#define DMX512_CHASER_DEFAULT {0, NULL, CHASER_STEP_MODE_LOOP, CHASER_STEP_DIRECTION_FORWARD, 0}
 
-typedef DMX512_engine_err_e (*chaserAddFunc)(DMX512_chaser_s *, DMX512_scene_s *);
-typedef DMX512_engine_err_e (*chaserClrFunc)(DMX512_chaser_s *, uint16_t);
-typedef void  (*trigFunc)(DMX512_chaser_s *);
+typedef enum{
+	CHASER_STEP_MODE_LOOP,
+	CHASER_STEP_MODE_SINGLE_SHOT,
+	CHASER_STEP_MODE_PING_PONG,
+	CHASER_STEP_MODE_RANDOM
+}DMX512_chaser_step_mode;
 
-typedef struct DMX512_chaser{
+typedef enum{
+	CHASER_STEP_DIRECTION_FORWARD,
+	CHASER_STEP_DIRECTION_BACKWARD
+}DMX512_chaser_step_direction;
+
+typedef struct{
 	uint16_t id;
-	uint16_t sceneCount;
-	DMX512_scene_s **scenes;
-	chaserAddFunc add;
-	chaserClrFunc clr;
-	trigFunc trigger;
+	DMX512_chaser_step_s *steps;
+	DMX512_chaser_step_mode mode;
+	DMX512_chaser_step_direction dir;
+	uint16_t step_count;
 }DMX512_chaser_s;
 
-
-DMX512_chaser_s *DMX512_chaser_init(DMX512_chaser_s *this, uint16_t id);
+DMX512_chaser_s DMX512_chaser_init(uint16_t id, DMX512_chaser_step_mode mode, DMX512_chaser_step_direction dir);
+DMX512_engine_err_e DMX512_chaser_add(DMX512_chaser_s *this, DMX512_chaser_step_s step);
+DMX512_engine_err_e DMX512_chaser_del(DMX512_chaser_s *this, uint16_t id);
+DMX512_fixture_preset_s *DMX512_chaser_get(DMX512_chaser_s *this, uint16_t id);
 
 #endif
