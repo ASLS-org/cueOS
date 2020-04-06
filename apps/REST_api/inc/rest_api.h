d8069d1370b2988e9ca908bb6b3625ea0d2e8c5c
Timé Kadel <time.kadel@reseau.eseo.fr>
2020-04-06 23:18:37 +0200

    [~] - Project architecture changes, removed nested resources in apps folder

---
+++ apps/REST_api/inc/rest_api.h
@@ -0,0 +1,22 @@
+/**
+ * @ingroup apps
+ * @defgroup rest_api
+ * REST API web services enabling configuration of the system
+ * through HTTP requests over pre-defined specific API enpoints.
+ * @{
+ */
+
+#ifndef REST_API_H__
+#define REST_API_H__
+
+#define REST_API_PORT 8000
+
+void rest_api_start(void);
+
+#endif
+
+/**
+ * @} Grouping in rest_api submodule ends
+ * @} Grouping in apps module ends
+ **/
+
---
+++ apps/REST_api/src/rest_api.c
@@ -0,0 +1,43 @@
+/**============================================================================================================================
+ * Depedencies inclusion
+ * Necessary dependencies should be declared here. Header file should contain as little dependecies declarations as possible
+ *=============================================================================================================================*/
+
+#include <string.h>
+#include "http_server.h"
+#include "DMX512_rest_api.h"
+#include "rest_api.h"
+
+
+/***============================================================================================================================
+ * Private functions definitions
+ * These functions are only accessible from within the file's scope
+ *=============================================================================================================================*/
+
+/**
+ * @brief routes HTTP requests received on API port
+
+ * @param req the request to be routed
+ */
+static void _rest_api_router(http_request_s *req){
+#if cueOS_CONFIG_NODETYPE == cueOS_NODETYPE_SLAVE_DMX
+	DMX512_rest_api_router(req);
+#elif cueOS_CONFIG_NODETYPE == cueOS_NODETYPE_MASTER
+
+#endif
+
+}
+
+
+/***============================================================================================================================
+ * Public functions definitions
+ * These functions can be accessed outside of the file's scope
+ * @see http_request.h for declarations
+ *=============================================================================================================================*/
+
+/**
+ * @brief Starts API HTTP server on port 8000.
+ */
+void rest_api_start(void){
+	http_server_init(REST_API_PORT, _rest_api_router);
+}
