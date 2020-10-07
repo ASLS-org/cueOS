787290ad2a257c3c7d82c36b949e7b000c33f23f
Timé Kadel <time.kadel@reseau.eseo.fr>
2020-10-07 15:46:34 +0200

    [+] - Implemented High Level I2C driver for I2C3 instance

---
+++ drivers/hl_drivers/serial/I2C/inc/I2C_driver.h
@@ -0,0 +1,13 @@
+/*
+ * I2C_driver.h
+ *
+ *  Created on: 6 Oct 2020
+ *      Author: root
+ */
+
+#ifndef INC_I2C_DRIVER_H_
+#define INC_I2C_DRIVER_H_
+
+
+
+#endif /* INC_I2C_DRIVER_H_ */
---
+++ drivers/hl_drivers/serial/I2C/src/I2C_driver.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* @brief I2C MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hi2c: I2C handle pointer
+* @retval None
+*/
+void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hi2c->Instance==I2C3)
+  {
+  /* USER CODE BEGIN I2C3_MspInit 0 */
+
+  /* USER CODE END I2C3_MspInit 0 */
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**I2C3 GPIO Configuration
+    PC9     ------> I2C3_SDA
+    PA8     ------> I2C3_SCL
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C3;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C3;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C3_CLK_ENABLE();
+//    /* I2C3 interrupt Init */
+//    HAL_NVIC_SetPriority(I2C3_EV_IRQn, 5, 0);
+//    HAL_NVIC_EnableIRQ(I2C3_EV_IRQn);
+  /* USER CODE BEGIN I2C3_MspInit 1 */
+
+  /* USER CODE END I2C3_MspInit 1 */
+  }
+
+}
